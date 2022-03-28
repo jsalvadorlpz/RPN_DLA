ef06f31703a879815581c24a8f939aaf4bbdf619
jsalvadorlpz <100415321+jsalvadorlpz@users.noreply.github.com>
2022-03-28 17:37:06 +0200

    Update config.py

--- detectron2/projects/vovnet-detectron2/vovnet/config.py
+++ detectron2/projects/vovnet-detectron2/vovnet/config.py
@@ -21,3 +21,25 @@ def add_vovnet_config(cfg):
     _C.MODEL.VOVNET.OUT_CHANNELS = 256
 
     _C.MODEL.VOVNET.BACKBONE_OUT_CHANNELS = 256
+
+    _C = cfg
+
+    _C.DATASETS.DLA = CN()
+
+    _C.DATASETS.DLA.REGISTER_NEW_DATASET = False
+
+    _C.DATASETS.DLA.TRAIN = CN()
+
+    _C.DATASETS.DLA.TRAIN.NAME = "my_dataset_train"
+
+    _C.DATASETS.DLA.TRAIN.JSON = "instances_train.json"
+
+    _C.DATASETS.DLA.TRAIN.PATH = "./datasets/train/"
+
+    _C.DATASETS.DLA.TEST = CN()
+
+    _C.DATASETS.DLA.TEST.NAME = "my_dataset_test"
+
+    _C.DATASETS.DLA.TEST.JSON = "instances_test.json"
+
+    _C.DATASETS.DLA.TEST.PATH = "./datasets/test/"
