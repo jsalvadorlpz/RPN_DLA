75a88dfe88df8e08258cd10b4f68583e4b228f34
jsalvadorlpz <100415321+jsalvadorlpz@users.noreply.github.com>
2022-03-28 17:25:28 +0200

    Update train_net.py

--- detectron2/projects/vovnet-detectron2/train_net.py
+++ detectron2/projects/vovnet-detectron2/train_net.py
@@ -1,4 +1,3 @@
-# Copyright (c) Youngwan Lee (ETRI) All Rights Reserved.
 """
 VoVNet Training Script.
 
@@ -6,21 +5,28 @@ This script is a simplified version of the training script in detectron2/tools.
 """
 
 import os
+import logging
+from collections import OrderedDict
+import torch
 
 import detectron2.utils.comm as comm
 from detectron2.checkpoint import DetectionCheckpointer
 from detectron2.config import get_cfg
-from detectron2.engine import DefaultTrainer, default_argument_parser, default_setup, launch
+from detectron2.data import MetadataCatalog
+from detectron2.data.datasets import register_coco_instances
+from detectron2.engine import DefaultTrainer, default_argument_parser, default_setup,hooks ,launch
 from detectron2.evaluation import (
-    COCOEvaluator, 
+    COCOEvaluator,
+    CityscapesInstanceEvaluator,
+    CityscapesSemSegEvaluator,
+    LVISEvaluator,
     COCOPanopticEvaluator,
     SemSegEvaluator,
     DatasetEvaluators,
     verify_results
     )
-from detectron2.data import MetadataCatalog
-
 from vovnet import add_vovnet_config
+
 
 
 class Trainer(DefaultTrainer):
@@ -45,6 +51,21 @@ class Trainer(DefaultTrainer):
         if evaluator_type == "coco_panoptic_seg":
             evaluator_list.append(COCOPanopticEvaluator(dataset_name, output_folder))
 
+        if evaluator_type == "cityscapes_instance":
+            assert (
+                torch.cuda.device_count() >= comm.get_rank()
+            ), "CityscapesEvaluator currently do not work with multiple machines."
+            return CityscapesInstanceEvaluator(dataset_name)
+        if evaluator_type == "cityscapes_sem_seg":
+            assert (
+                torch.cuda.device_count() >= comm.get_rank()
+            ), "CityscapesEvaluator currently do not work with multiple machines."
+            return CityscapesSemSegEvaluator(dataset_name)
+        elif evaluator_type == "pascal_voc":
+            return PascalVOCDetectionEvaluator(dataset_name)
+        elif evaluator_type == "lvis":
+            return LVISEvaluator(dataset_name, cfg, True, output_folder)
+
         if len(evaluator_list) == 0:
             raise NotImplementedError(
                 "no Evaluator for the dataset {} with the type {}".format(
@@ -53,10 +74,26 @@ class Trainer(DefaultTrainer):
             )
         elif len(evaluator_list) == 1:
             return evaluator_list[0]
-        
         return DatasetEvaluators(evaluator_list)
 
 
+    @classmethod
+    def test_with_TTA(cls, cfg, model):
+        logger = logging.getLogger("detectron2.trainer")
+        # In the end of training, run an evaluation with TTA
+        # Only support some R-CNN models.
+
+        logger.info("Running inference with test-time augmentation ...")
+        model = GeneralizedRCNNWithTTA(cfg, model)
+        evaluators = [
+            cls.build_evaluator(
+                cfg, name, output_folder=os.path.join(cfg.OUTPUT_DIR, "inference_TTA")
+            )
+            for name in cfg.DATASETS.TEST
+        ]
+        res = cls.test(cfg, model, evaluators)
+        res = OrderedDict({k + "_TTA": v for k, v in res.items()})
+        return res
 def setup(args):
     """
     Create configs and perform basic setups.
@@ -69,9 +106,17 @@ def setup(args):
     default_setup(cfg, args)
     return cfg
 
-
 def main(args):
     cfg = setup(args)
+    #--- add datasets on the fly using coco format
+    if cfg.DATASETS.DLA.REGISTER_NEW_DATASET == True:
+        register_coco_instances(cfg.DATASETS.DLA.TRAIN.NAME, {},
+                cfg.DATASETS.DLA.TRAIN.JSON,
+                cfg.DATASETS.DLA.TRAIN.PATH)
+        register_coco_instances(cfg.DATASETS.DLA.TEST.NAME, {},
+                cfg.DATASETS.DLA.TEST.JSON,
+                cfg.DATASETS.DLA.TEST.PATH)
+
 
     if args.eval_only:
         model = Trainer.build_model(cfg)
@@ -79,14 +124,24 @@ def main(args):
             cfg.MODEL.WEIGHTS, resume=args.resume
         )
         res = Trainer.test(cfg, model)
+        if cfg.TEST.AUG.ENABLED:
+            res.update(Trainer.test_with_TTA(cfg, model))
         if comm.is_main_process():
             verify_results(cfg, res)
         return res
 
+    """
+    If you'd like to do anything fancier than the standard training logic,
+    consider writing your own training loop (see plain_train_net.py) or
+    subclassing the trainer.
+    """
     trainer = Trainer(cfg)
     trainer.resume_or_load(resume=args.resume)
+    if cfg.TEST.AUG.ENABLED:
+        trainer.register_hooks(
+            [hooks.EvalHook(0, lambda: trainer.test_with_TTA(cfg, trainer.model))]
+        )
     return trainer.train()
-
 
 if __name__ == "__main__":
     args = default_argument_parser().parse_args()
@@ -98,4 +153,5 @@ if __name__ == "__main__":
         machine_rank=args.machine_rank,
         dist_url=args.dist_url,
         args=(args,),
-    )
+        )
+
