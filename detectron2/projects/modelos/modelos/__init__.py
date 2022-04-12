d78f5ac3429ba1372f14dec99b84b73ec2072bbb
jsalvadorlpz <100415321+jsalvadorlpz@users.noreply.github.com>
2022-04-12 09:03:25 +0200

    Update __init__.py

--- detectron2/projects/modelos/modelos/__init__.py
+++ detectron2/projects/modelos/modelos/__init__.py
@@ -1,3 +1,2 @@
-from .config import add_vovnet_config
-from .vovnet import build_vovnet_fpn_backbone, build_vovnet_backbone
+from .config import add_mobilnetv2_config
 from .mobilenet import build_mobilenetv2_fpn_backbone, build_mnv2_backbone
