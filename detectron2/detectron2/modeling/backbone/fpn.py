207463d6765407621a77e99f43f063a966856312
jsalvadorlpz <100415321+jsalvadorlpz@users.noreply.github.com>
2022-04-26 19:38:13 +0200

    Update fpn.py

--- detectron2/detectron2/modeling/backbone/fpn.py
+++ detectron2/detectron2/modeling/backbone/fpn.py
@@ -253,3 +253,25 @@ def build_retinanet_resnet_fpn_backbone(cfg, input_shape: ShapeSpec):
         fuse_type=cfg.MODEL.FPN.FUSE_TYPE,
     )
     return backbone
+
+@BACKBONE_REGISTRY.register()
+def build_resnet_mod_fpn_backbone(cfg, input_shape: ShapeSpec):
+    """
+    Args:
+        cfg: a detectron2 CfgNode
+
+    Returns:
+        backbone (Backbone): backbone module, must be a subclass of :class:`Backbone`.
+    """
+    bottom_up = build_resnet_mod_backbone(cfg, input_shape)
+    in_features = cfg.MODEL.FPN.IN_FEATURES
+    out_channels = cfg.MODEL.FPN.OUT_CHANNELS
+    backbone = FPN(
+        bottom_up=bottom_up,
+        in_features=in_features,
+        out_channels=out_channels,
+        norm=cfg.MODEL.FPN.NORM,
+        top_block=LastLevelMaxPool(),
+        fuse_type=cfg.MODEL.FPN.FUSE_TYPE,
+    )
+    return backbone
